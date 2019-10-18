2a28c4b11ab505a2c3f2f5485a5bf35bfd077598
Jason <jasonrizzo02@aol.com>
2019-10-17 21:27:18 -0400

    Add project files.

---
+++ LabTwo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LabTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Welcome to Grand Circus’ Room Detail Generator!" + "\n" + "Enter Length:");
+                double length = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Width:");
+                double width = double.Parse(Console.ReadLine());
+
+                double area = length * width;
+                double perimeter = 2 * area;
+
+                Console.WriteLine(area);
+                Console.WriteLine(perimeter);
+
+                Console.WriteLine("Continue? y/n");
+                string answer = Console.ReadLine();
+
+                if (answer == "y" || answer == "Y")
+                {
+
+                }
+                else
+                {
+
+
+                }
+
+
+            } while (true);
+        }
+    }
+}
